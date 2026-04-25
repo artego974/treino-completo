fc88d08a225278419c14ef14f06cbb71d2cb01c8
artego974 <artego974@gmail.com>
2026-04-25 10:09:35 -0300

    ajuste do jwt

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -1,14 +1,14 @@
 import jwt from "jsonwebtoken"
-import dotenv from "dotenv"
+import * as dotenv from "dotenv"
 
 dotenv.config()
 
 export const generateToken = (payload:object)=>{
-    return jwt.sign(payload, process.env.JWT_SECRET, {
+    return jwt.sign(payload, String(process.env.JWT_SECRET), {
       expiresIn: Number(process.env.JWT_EXPIRE),
     });
 }
 
 export const validToken = (token:string)=>{
-    return jwt.verify(token, process.env.JWT_SECRET);
+    return jwt.verify(token, String(process.env.JWT_SECRET));
 }
